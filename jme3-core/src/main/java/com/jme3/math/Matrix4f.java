7360225e3cfb305bb1918c7d4a10610d6b166956
CORP\fdenijs <friso.denijs@capgemini.com>
2016-10-28 10:04:11 +0200

    changed matrix constructor

--- jme3-core/src/main/java/com/jme3/math/Matrix4f.java
+++ jme3-core/src/main/java/com/jme3/math/Matrix4f.java
@@ -75,38 +75,12 @@ public final class Matrix4f implements Savable, Cloneable, java.io.Serializable 
     }
 
     /**
-     * constructs a matrix with the given values.
-     */
-    public Matrix4f(float m00, float m01, float m02, float m03,
-            float m10, float m11, float m12, float m13,
-            float m20, float m21, float m22, float m23,
-            float m30, float m31, float m32, float m33) {
-
-        this.m00 = m00;
-        this.m01 = m01;
-        this.m02 = m02;
-        this.m03 = m03;
-        this.m10 = m10;
-        this.m11 = m11;
-        this.m12 = m12;
-        this.m13 = m13;
-        this.m20 = m20;
-        this.m21 = m21;
-        this.m22 = m22;
-        this.m23 = m23;
-        this.m30 = m30;
-        this.m31 = m31;
-        this.m32 = m32;
-        this.m33 = m33;
-    }
-
-    /**
      * Create a new Matrix4f, given data in column-major format.
      *
      * @param array
      *		An array of 16 floats in column-major format (translation in elements 12, 13 and 14).
      */
-    public Matrix4f(float[] array) {
+    public Matrix4f(float... array) {
         set(array, false);
     }
 
